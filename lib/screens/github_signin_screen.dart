e4f05e580fbbe5c9d31d74058ff561abd77b27bc
Degamisu <degamisu@gmail.com>
2024-02-02 20:23:45 -0500

    upload

--- lib/screens/github_signin_screen.dart
+++ lib/screens/github_signin_screen.dart
@@ -1,10 +1,9 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class GitHubSignInScreen extends StatelessWidget {
-  final String clientId = 'dbee0b940a5d97bab2b5';
-  final String redirectUri = 'http://localhost:3000/callback';
-  final String authorizationUrl = 'https://github.com/login/oauth/authorize?client_id=dbee0b940a5d97bab2b5&scope=user';
+class _GitHubSignInScreenState extends State<GitHubSignInScreen> {
+  String? _code;
 
   @override
   Widget build(BuildContext context) {
@@ -16,35 +15,55 @@ class GitHubSignInScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildWebView() {
-    return WebView(
-      initialUrl: authorizationUrl,
-      javascriptMode: JavascriptMode.unrestricted,
-      navigationDelegate: (NavigationRequest request) {
-        if (request.url.startsWith(redirectUri)) {
-          // Extract code from the callback URL
-          Uri uri = Uri.parse(request.url);
-          String code = uri.queryParameters['code'] ?? '';
+    Widget _buildWebView() {
+    if (Platform.isLinux) {
+        // Handle Linux differently or choose an alternative approach
+        return Center(
+        child: Text('GitHub Sign In is not supported on Linux.'),
+        );
+    } else {
+        return WebView(
+        initialUrl: widget.authorizationUrl,
+        javascriptMode: JavascriptMode.unrestricted,
+        navigationDelegate: (NavigationRequest request) {
+            if (request.url.startsWith(widget.redirectUri)) {
+            Uri uri = Uri.parse(request.url);
+            String code = uri.queryParameters['code'] ?? '';
 
-          // Use a closure to capture the code value
-          _handleGitHubCallback(context, () {
-            return code;
-          });
+            setState(() {
+                _code = code;
+            });
 
-          return NavigationDecision.prevent;
-        }
-        return NavigationDecision.navigate;
-      },
-    );
+            return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+        },
+        );
+    }
+    }
+
+  void _handleGitHubCallback() {
+    if (_code != null) {
+      // Now you can use the '_code' variable as needed
+
+      Navigator.pop(context); // Close the GitHubSignInScreen
+      // Navigate to the next screen or perform necessary actions
+    }
   }
 
-  void _handleGitHubCallback(BuildContext context, Function() getCode) {
-    // Implement code handling (send it to your server, etc.)
-    // Once authenticated, you can navigate to the next screen
-    String code = getCode();
-    // Now you can use the 'code' variable as needed
-
-    Navigator.pop(context); // Close the GitHubSignInScreen
-    // Navigate to the next screen or perform necessary actions
+  @override
+  void initState() {
+    super.initState();
+    _handleGitHubCallback();
   }
+}
+
+class GitHubSignInScreen extends StatefulWidget {
+  final String authorizationUrl;
+  final String redirectUri;
+
+  GitHubSignInScreen({required this.authorizationUrl, required this.redirectUri});
+
+  @override
+  _GitHubSignInScreenState createState() => _GitHubSignInScreenState();
 }
